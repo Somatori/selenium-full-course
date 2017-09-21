6bf662103f0127e971e54462887bd8b4c8c9f9a4
Иван Иванов <venustype@outlook.com>
2017-09-21 10:55:04 +0300

    The 'LitecartAdminLogin' test has been added

---
+++ Project1/Project1/Litecart-admin-1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace litecart
+{
+    [TestFixture]
+    public class LitecartAdmin1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void LitecartAdminLogin()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("a[href='http://localhost/litecart/admin/logout.php']")));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
